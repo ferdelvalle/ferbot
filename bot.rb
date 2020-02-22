699ee6714eec6c57df6e1780ad6c356b487fc955
Fernando Del Valle <ferdelvalle@me.com>
2020-02-21 21:40:47 -0600

    Makes the reccomendation logic
    Adds the  logic for recommending dishes
    
    it also adds rubocop and sticker files

--- bot.rb
+++ bot.rb
@@ -1,5 +1,5 @@
 require 'telegram/bot'
-# require_relative '../ferbot/ferbot/lib/logic'
+require_relative '../ferbot/ferbot/lib/logic'
 token = '877177203:AAE5tNPbYDt62BTzlWxG02FzYhe6uqQHu88'
 
 Telegram::Bot::Client.run(token) do |bot|
@@ -8,7 +8,9 @@ Telegram::Bot::Client.run(token) do |bot|
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/food'
+      recommendation = Dishes.recommend
       
+      bot.api.send_message(chat_id: message.chat.id, text: "What about #{recommendation[0]}? It's a great #{recommendation[1]} dish!")
 
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
